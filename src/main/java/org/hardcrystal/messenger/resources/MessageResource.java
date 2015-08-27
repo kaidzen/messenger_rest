bd3059f99c9dbff625bca4d5002602c5a033d166
Sergey Karpov <karpov.srg@gmail.com>
2015-08-27 19:33:25 +0300

    Implement POST API in JSON

--- src/main/java/org/hardcrystal/messenger/resources/MessageResource.java
+++ src/main/java/org/hardcrystal/messenger/resources/MessageResource.java
@@ -2,10 +2,7 @@ package org.hardcrystal.messenger.resources;
 
 import java.util.List;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 
 import org.hardcrystal.messenger.model.Message;
@@ -20,6 +17,13 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
+	}
+
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message addMessage(Message message){
+		return messageService.addMessage(message);
 	}
 
 	@GET
