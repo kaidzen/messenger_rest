b09a92dde3b77d74c025da17570b8fb59fea2adb
Sergey Karpov <karpov.srg@gmail.com>
2015-08-31 20:19:02 +0300

    Implement PUT and DELETE in methods (update, delete) Modifying class MessageResource annotations.

--- src/main/java/org/hardcrystal/messenger/resources/MessageResource.java
+++ src/main/java/org/hardcrystal/messenger/resources/MessageResource.java
@@ -9,26 +9,37 @@ import org.hardcrystal.messenger.model.Message;
 import org.hardcrystal.messenger.service.MessageService;
 
 @Path("/messages")
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
 public class MessageResource {
 	
 	MessageService messageService = new MessageService();
 	
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages(){
 		return messageService.getAllMessages();
 	}
 
 	@POST
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message addMessage(Message message){
 		return messageService.addMessage(message);
 	}
 
+	@PUT
+	@Path("/{messageId}")
+	public Message updateMessage(@PathParam("messageId")long id, Message message){
+		message.setId(id);
+		return messageService.updateMessage(message);
+	}
+
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId")long id){
+		messageService.removeMessage(id);
+	}
+
 	@GET
 	@Path("/{messageId}")
-	@Produces(MediaType.APPLICATION_JSON)
 	public Message test(@PathParam("messageId")long messageId){
 		return messageService.getMessage(messageId);
 	}
