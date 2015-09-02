4c15a8815e99eda8257fce2ba382fff2ebe44497
Sergey Karpov <karpov.srg@gmail.com>
2015-09-02 18:45:34 +0300

    ADD other type of param - @MatrixParam and @HeaderParam

---
+++ src/main/java/org/hardcrystal/messenger/resources/InjectDemoResourse.java
@@ -0,0 +1,17 @@
+package org.hardcrystal.messenger.resources;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+@Path("/injectdemo")
+@Consumes(MediaType.TEXT_PLAIN)
+@Produces(MediaType.APPLICATION_JSON)
+public class InjectDemoResourse {
+
+    @GET
+    @Path("annotations")
+    public String getParamsUsingAnnotations(@MatrixParam("param") String matrixParam,
+                                            @HeaderParam("authSessionID") String header){
+        return "Matrix param: " + matrixParam + " Header param: " + header;
+    }
+}
